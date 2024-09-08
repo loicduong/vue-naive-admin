efbe9cb22a1d8b6df1782c4ca8f934d17ecd3e8a
loicduong <mikhailcolt@gmail.com>
2024-09-08 12:08:21 +0700

    feat(core): add runtime config

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,4 +1,5 @@
 import json5 from 'json5'
+import { getEnvVariable } from './env'
 
 /**
  * Create service config by current env
@@ -6,18 +7,16 @@ import json5 from 'json5'
  * @param env The current env
  */
 export function createServiceConfig(env: Env.ImportMeta) {
-  const { VITE_SERVICE_BASE_URL, VITE_OTHER_SERVICE_BASE_URL } = env
-
   let other = {} as Record<App.Service.OtherBaseURLKey, string>
   try {
-    other = json5.parse(VITE_OTHER_SERVICE_BASE_URL)
+    other = json5.parse(getEnvVariable('VITE_OTHER_SERVICE_BASE_URL', env))
   }
   catch {
     console.error('VITE_OTHER_SERVICE_BASE_URL is not a valid json5 string')
   }
 
   const httpConfig: App.Service.SimpleServiceConfig = {
-    baseURL: VITE_SERVICE_BASE_URL,
+    baseURL: getEnvVariable('VITE_SERVICE_BASE_URL', env),
     other,
   }
 
