009215687d23a3292c8463a171b5cdafd4915105
loicduong <loicduong@users.noreply.github.com>
2025-01-17 12:44:06 +0700

    feat(projects): add pwa

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevtools from 'vite-plugin-vue-devtools'
 import { setupHtmlPlugin } from './html'
+import { setupPwaPlugin } from './pwa'
 import { setupRouter } from './router'
 import { setupUnocss } from './unocss'
 import { setupUnplugin } from './unplugin'
@@ -16,6 +17,7 @@ export function setupVitePlugins(viteEnv: Env.ImportMeta, buildTime: string, bui
     setupUnocss(viteEnv),
     ...setupUnplugin(viteEnv),
     setupHtmlPlugin(buildTime, buildVersion),
+    setupPwaPlugin(),
   ]
 
   return plugins
