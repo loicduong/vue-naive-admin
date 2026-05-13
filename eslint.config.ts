116317dbe03226fa958591d4f5edf1c3c6226eaf
loicduong <mikhailcolt@gmail.com>
2026-05-13 23:22:54 +0700

    chore: simplify eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,22 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu(
   {
     formatters: true,
-    regexp: false,
     unocss: true,
     vue: true,
-  },
-  {
-    rules: {
-      '@typescript-eslint/consistent-type-definitions': 'off',
-      'ts/no-unsafe-function-type': 'off',
-      'unocss/order-attributify': 'off',
-      'vue/max-attributes-per-line': [
-        'error',
-        {
-          singleline: { max: Infinity },
-          multiline: { max: 1 },
-        },
-      ],
-    },
   },
 )
