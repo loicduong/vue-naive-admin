2d0e38d8aad115b113aac84f85ee882828a12366
loicduong <mikhailcolt@gmail.com>
2026-05-13 22:23:06 +0700

    ci: add semantic-release changelog and version bump

--- release.config.cjs
+++ release.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: `chore(release): \${nextRelease.version} [skip ci]
+
+\${nextRelease.notes}`,
+      },
+    ],
     [
       '@semantic-release/github',
       {
